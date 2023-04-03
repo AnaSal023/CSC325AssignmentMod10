a4871cf5ed00a8a5f6aecb915148a6373f1841e6
anasa <anasa@LAPTOP-FPAKTMJC.lan>
2023-04-03 16:29:22 -0400

    created casual class that implements products interface and its methods

---
+++ src/main/java/com/anasalazar/assignmentmodule10/Casual.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.anasalazar.assignmentmodule10;
+
+/**
+ *
+ * @author anasa
+ */
+public class Casual implements Products{
+
+    @Override
+    public String getTops() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getShoes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getPants() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
